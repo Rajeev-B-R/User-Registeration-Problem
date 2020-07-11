eacacc10ed60af71e7ff2312591a41359ed4c240
Rajeev <rajeevsuchiha@gmail.com>
2020-07-12 00:49:23 +0530

    Added:Code to accept valid first name added

--- userRegProb.sh
+++ userRegProb.sh
@@ -1,3 +1,15 @@
 echo "**********Welcome to User Registration Problem**********"
 
+echo "Enter your first name:"
+read firstName
+
+pattern='^[A-Z]{1}[a-z]{2,}$'
+
+if [[ $firstName =~ $pattern ]]
+then
+	echo "Valid First Name!"
+else
+	echo "Invalid first name!"
+fi
+
 
